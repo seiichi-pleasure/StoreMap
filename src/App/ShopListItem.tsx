172db540fcc71790cf4be0bdd63b40317998f2ce
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-19 16:40:13 +0900

    カテゴリ→法人

--- src/App/ShopListItem.tsx
+++ src/App/ShopListItem.tsx
@@ -15,7 +15,7 @@ const Content = (props: Props) => {
   }
 
   const distanceTipText = makeDistanceLabelText(props.data.distance)
-  const category = props.data['O/O']
+  const category = props.data['法人']
   const image = props.data['画像']
 
 
