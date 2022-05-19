a17135fdee6d6c71c79da313ba1cff1cd1ca3972
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-19 15:15:25 +0900

    カテゴリ→O/O

--- src/App/List.tsx
+++ src/App/List.tsx
@@ -26,7 +26,7 @@ const Content = (props: Props) => {
 
     if (queryCategory) {
       data = props.data.filter((shop) => {
-        return shop['カテゴリ'] === queryCategory
+        return shop['O/O'] === queryCategory
       })
     }
 
