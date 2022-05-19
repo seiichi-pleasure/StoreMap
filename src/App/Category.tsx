0d9d755eac51c5ab77eba7d75ca123b25447d7ec
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-19 17:04:56 +0900

    カテゴリ

--- src/App/Category.tsx
+++ src/App/Category.tsx
@@ -20,9 +20,9 @@ const Content = (props: Props) => {
     for (let i = 0; i < props.data.length; i++) {
       const shop = props.data[i];
 
-      if (categories.indexOf(shop['法人']) === -1) {
+      if (categories.indexOf(shop['カテゴリ']) === -1) {
 
-        categories.push(shop['法人'])
+        categories.push(shop['カテゴリ'])
       }
 
     }
