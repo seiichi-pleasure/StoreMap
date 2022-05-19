5143a990add21a2428fc046fbd6248b66dc6bbc7
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-19 15:53:08 +0900

    Update Shop.tsx

--- src/App/Shop.tsx
+++ src/App/Shop.tsx
@@ -91,12 +91,8 @@ const Content = (props: Props) => {
             <p><a className="small" href={`http://maps.apple.com/?q=${shop['緯度']},${shop['経度']}`}>店舗までのルート</a></p>
               <div className="small">
                 <ul>
-                  <li><div className="text">O/O or FSM</div><div>shop['O/O']</div></li>
-                  <li><div className="text">紹介文</div><div>shop['紹介文']</div></li>
-                  <li><div className="text">O/O or FSM</div><div>shop['O/O']</div></li>
-                  <li><div className="text">紹介文</div><div>shop['紹介文']</div></li>
-                  <li><div className="text">O/O or FSM</div><div>shop['O/O']</div></li>
-                  <li><div className="text">紹介文</div><div>shop['紹介文']</div></li>
+                  <li><div>O/O or FSM</div><div>{shop['O/O']}</div></li>
+                  <li><div>紹介文</div><div>{shop['紹介文']}</div></li>
                 </ul>
               </div>
 
