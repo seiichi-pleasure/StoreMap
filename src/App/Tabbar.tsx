c6d778a1dbd0240e146835b664e4ad68154c11d4
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-19 14:38:50 +0900

    Update Tabbar.tsx

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './Tabbar.scss'
 
-import { FaList, FaHome, FaSearch, FaCamera } from "react-icons/fa"
-import { AiOutlineAppstore } from "react-icons/ai"
+import { FaList, FaHome, FaSearch } from "react-icons/fa"
+//import { FaList, FaHome, FaSearch, FaCamera } from "react-icons/fa"
+//import { AiOutlineAppstore } from "react-icons/ai"
 
 const Content = () => {
   return (
@@ -12,8 +13,8 @@ const Content = () => {
         <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
         <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
         <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">カテゴリ</div></Link></li>
-        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">写真から探す</div></Link></li>
-        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
+//        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">写真から探す</div></Link></li>
+//        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
       </ul>
     </div>
   );
