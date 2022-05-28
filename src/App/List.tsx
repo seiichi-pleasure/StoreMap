7961020240d2cf5734eef30d2b86706c596b1c67
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-28 20:55:58 +0900

    Update List.tsx

--- src/App/List.tsx
+++ src/App/List.tsx
@@ -119,6 +119,7 @@ const Content = (props: Props) => {
                   data={item}
                   popupHandler={popupHandler}
                   queryCategory={queryCategory}
+                  queryStore={queryStore}
                 />
               </div>)
 
