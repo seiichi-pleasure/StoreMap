2785bfa65a62b65b354e638f66b4e22b32c3e4e8
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-29 22:21:11 +0900

    category

--- src/App/Store.tsx
+++ src/App/Store.tsx
@@ -36,7 +36,7 @@ const Content = (props: Props) => {
             <Select
               onChange={(e) => {
                 if (e) {
-                  navigate(`/list?store=${e.value}`);
+                  navigate(`/list?category =${e.value}`);
                 }
               }}
               options={
