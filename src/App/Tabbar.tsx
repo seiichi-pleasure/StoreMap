3eead57448edfa136e6badbad7daa4b6091d99b3
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-29 21:07:22 +0900

    store search

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -9,9 +9,9 @@ const Content = () => {
     <div className="tabbar">
       <ul>
         <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
-        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
         <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">O/O</div></Link></li>
-        <li><Link to="/store"><div className="icon"><FaSearch /></div><div className="text">店舗</div></Link></li>
+        <li><Link to="/store"><div className="icon"><FaStoreSearch /></div><div className="text">店舗</div></Link></li>
+        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
       </ul>
     </div>
   );
