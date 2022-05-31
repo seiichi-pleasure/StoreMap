eed79b514f2eff0230149c81e91ef9792ff0a76e
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-31 18:33:57 +0900

    店舗名

--- src/App/ShopListItem.tsx
+++ src/App/ShopListItem.tsx
@@ -24,7 +24,7 @@ const Content = (props: Props) => {
   return (
     <>
       <div className="shop-link">
-        <h2 className="shop-title" onClick={clickHandler}>{props.data['スポット名']}</h2>
+        <h2 className="shop-title" onClick={clickHandler}>{props.data['店舗名']}</h2>
         <div className='tag-box'>
           <span className="nowrap">
             {
@@ -39,7 +39,7 @@ const Content = (props: Props) => {
 
         <div style={{ margin: "10px 10px 10px 0" }}>
 
-          { image && <img src={image} alt={props.data['スポット名']} onClick={clickHandler}/>}
+          { image && <img src={image} alt={props.data['店舗名']} onClick={clickHandler}/>}
 
         </div>
 
