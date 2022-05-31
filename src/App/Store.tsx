09687b94343e03361cc50ca2019175f43b26f239
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-31 18:33:30 +0900

    店舗名

--- src/App/Store.tsx
+++ src/App/Store.tsx
@@ -18,7 +18,7 @@ const Content = (props: Props) => {
 
     for (let i = 0; i < props.data.length; i++) {
       const shop = props.data[i];
-      if (stores.indexOf(shop['スポット名']) === -1) stores.push(shop['スポット名']);
+      if (stores.indexOf(shop['店舗名']) === -1) stores.push(shop['店舗名']);
     }
 
     setStoreList(stores)
