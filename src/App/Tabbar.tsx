16c6fc7c91f28f82e8ac1fc8a887fca60341d0b1
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-05-31 14:06:48 +0900

    prefecture

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -12,6 +12,7 @@ const Content = () => {
         <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">O/O</div></Link></li>
         <li><Link to="/store"><div className="icon"><FaSearch /></div><div className="text">店舗</div></Link></li>
         <li><Link to="/bc"><div className="icon"><FaSearch /></div><div className="text">BC</div></Link></li>
+        <li><Link to="/prefecture"><div className="icon"><FaSearch /></div><div className="text">都道府県</div></Link></li>
         <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
       </ul>
     </div>
