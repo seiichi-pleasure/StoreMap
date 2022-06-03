924a74721401ecd09c18a2b02ac658f81308c96e
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-06-03 12:15:22 +0900

    Update Container.tsx

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import App from './App'
-import About from './About'
-import './Container.scss'
+// import About from './About'
+// import './Container.scss'
 
 function Content() {
   return (
