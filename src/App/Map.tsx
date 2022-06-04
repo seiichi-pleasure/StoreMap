7bc6375950f32097db6051fd9bdf27bebfc18be9
seiichi-pleasure <105725121+seiichi-pleasure@users.noreply.github.com>
2022-06-04 20:36:31 +0900

    Update Map.tsx

--- src/App/Map.tsx
+++ src/App/Map.tsx
@@ -66,24 +66,6 @@ const Content = (props: Props) => {
         clusterRadius: 25,
       })
 
-      if ('{店舗名}' === '千里丘店'){
-      mapObject.addLayer({
-        id: 'shop-points',
-        type: 'circle',
-        source: 'shops',
-        filter: ['all',
-          ['==', '$type', 'Point'],
-        ],
-        paint: {
-          'circle-radius': 13,
-          'circle-color': '#0000FF',
-          'circle-opacity': 0.4,
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#FFFFFF',
-          'circle-stroke-opacity': 1,
-        },
-      })
-      }else{
       mapObject.addLayer({
         id: 'shop-points',
         type: 'circle',
@@ -99,36 +81,6 @@ const Content = (props: Props) => {
           'circle-stroke-color': '#FFFFFF',
           'circle-stroke-opacity': 1,
         },
-      })
-      }
-
-      mapObject.addLayer({
-        id: 'shop-symbol',
-        type: 'symbol',
-        source: 'shops',
-        filter: ['all',
-          ['==', '$type', 'Point'],
-        ],
-        paint: {
-          'text-color': textColor,
-          'text-halo-color': textHaloColor,
-          'text-halo-width': 2,
-        },
-        layout: {
-          'text-field': "{店舗名}{O/O}",
-          'text-font': ['Noto Sans Regular'],
-          'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
-          'text-radial-offset': 0.5,
-          'text-justify': 'auto',
-          'text-size': 12,
-          'text-anchor': 'top',
-          'text-max-width': 12,
-          'text-allow-overlap': false,
-        },
-      })
-
-      mapObject.on('mouseenter', 'shop-points', () => {
-        mapObject.getCanvas().style.cursor = 'pointer'
       })
 
       mapObject.on('mouseleave', 'shop-points', () => {
